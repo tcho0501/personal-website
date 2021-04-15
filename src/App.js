e00431347f05a9c054d17886dfd46ebc106b79bc
Tim Cho <timcho@umich.edu>
2021-04-15 19:47:00 +0800

    updated deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tim's personal website
         </a>
       </header>
     </div>
